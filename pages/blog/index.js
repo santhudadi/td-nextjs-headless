85cfcc2788c9a524eaeddeec08a4027d9af9a6a5
Sunny Dadi <santhudadi@hotmail.com>
2024-04-21 21:26:15 +1000

    initial commit

---
+++ pages/blog/index.js
@@ -0,0 +1,7 @@
+export default function Blog() {
+    return (
+        <div>
+            <h1>Blog Index Page</h1>
+        </div>
+    );
+}
---
+++ pages/category/[categoryName].js
@@ -0,0 +1,7 @@
+export default function CategoryArchive() {
+    return (
+        <div>
+            <h1>Category Archive Page</h1>
+        </div>
+    );
+}
